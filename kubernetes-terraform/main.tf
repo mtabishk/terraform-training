b833e1acd513a86282b92d5b675c6ee50bd3e89d
Muhammad Tabish Khanday <mtabishkhanday@gmail.com>
2021-06-02 22:19:20 +0530

    added kubernetes and terraform

---
+++ kubernetes-terraform/main.tf
@@ -0,0 +1,63 @@
+resource "kubernetes_deployment" "example" {
+  metadata {
+    name = "terraform-example"
+    labels = {
+      test = "MyExampleApp"
+    }
+  }
+
+  spec {
+    replicas = 3
+
+    selector {
+      match_labels = {
+        test = "MyExampleApp"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          test = "MyExampleApp"
+        }
+      }
+
+      spec {
+        container {
+          image = "mtabishk/webserver-apache-php"
+          name  = "example"
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "example" {
+  metadata {
+    name = "terraform-example"
+  }
+  spec {
+    selector = {
+      test = "MyExampleApp"
+    }
+    session_affinity = "ClientIP"
+    port {
+      port        = 80
+      target_port = 80
+      node_port   = 31000
+    }
+
+    type = "NodePort"
+  }
+}
---
+++ kubernetes-terraform/provider.tf
@@ -0,0 +1,4 @@
+provider "kubernetes" {
+  config_path    = "C:\\Users\\mtabishk\\.kube\\config"
+  config_context = "minikube"
+}
